4cefef17e4caa99397accd39ada3e93c13016c3d
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-16 08:48:57 +0900

    TOKEN  한 번만 읽는 것

--- publish.py
+++ publish.py
@@ -3,25 +3,21 @@ import argparse
 import subprocess
 
 ### ADD MEDIUM INTEGRATION TOKEN HERE ###
-TOKEN = ""
+
 
 class MediumPublisher:
 
-	@classmethod
-	def __read_token( self ):
-		f = open("token.ini", "r")
-		return f.read()
+	TOKEN = open("token.ini", "r").read()
 
 	@classmethod
 	def __get_header( self ):
-		token = self .__read_token()
 		return  {
 			"Accept":	"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 			"Accept-Encoding"	:"gzip, deflate, br",
 			"Accept-Language"	:"en-US,en;q=0.5",
 			"Connection"	:"keep-alive",
 			"Host"	:"api.medium.com",
-			"Authorization": "Bearer {}".format( token ),
+			"Authorization": f"Bearer {self.TOKEN}",
 			"Upgrade-Insecure-Requests":	"1",
 			"User-Agent":	"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
 		}
@@ -57,7 +53,7 @@ class MediumPublisher:
 	@classmethod
 	def get_author_id( self ):
 		'''uses the /me medium api endpoint to get the user's author id'''
-		response = requests.get("https://api.medium.com/v1/me", headers=self.__get_header(), params={"Authorization": "Bearer {}".format(TOKEN)})
+		response = requests.get("https://api.medium.com/v1/me", headers=self.__get_header(), params={"Authorization": f"Bearer {self.TOKEN}" })
 		if response.status_code == 200:
 			return response.json()['data']['id']
 		return None
