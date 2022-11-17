0066a418fa8e52d46a3eb9ed1feec80692674193
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-17 10:06:31 +0900

    convert_code_block_to_id 기능 추가
    1. convert_code_block_to_id 기능 추가 및 테스트 추가
    2. 필요 없는 Library 제거

--- gistCodeUploader.py
+++ gistCodeUploader.py
@@ -1,26 +1,44 @@
 #
+import re
+import ntpath
+
+
 class GistCodeUploader:
 
+	DELIMITER = "_@_"
+
 	@classmethod
-	def read_file( self, path ):
+	def exec(self, path):
+		content = self.read_file(path)
+		self.convert_code_block_to_id(content)
+
+	@classmethod
+	def read_file(self, path):
 		content = None
-		with open( path, 'r', encoding='utf-8') as f:
+		with open(path, 'r', encoding='utf-8') as f:
 			content = f.read()
 		return content
 
 	@classmethod
-	def convert_code_block_to_id( self, markdown_text ):
+	def convert_code_block_to_id(self, file_base_name, markdown_text):
 		id_2_code_block_info_dict = {}
 
-		return
+		temp_markdown_text = markdown_text
+		code_block_arr = re.findall('```[\s\S][^```]+```', markdown_text)
+		for index, code_block in enumerate(code_block_arr):
+			id = f'{self.DELIMITER}{file_base_name}_code_{index}{self.DELIMITER}'
+			temp_markdown_text = temp_markdown_text.replace(code_block, id)
+			id_2_code_block_info_dict[id] = code_block
+
+		return id_2_code_block_info_dict, temp_markdown_text
 
 	@classmethod
-	def save_temp_markdown_file( self, temp_markdown_text ):
+	def save_temp_markdown_file(self, temp_markdown_text):
 		path = None
 		return path
 
 	@classmethod
-	def upload_code_block_to_gist( self, id_2_code_block_info_dict ):
+	def upload_code_block_to_gist(self, id_2_code_block_info_dict):
 		id_2_gist_link_dict = {}
 
 		return id_2_gist_link_dict
--- gistCodeUploaderTest.py
+++ gistCodeUploaderTest.py
@@ -1,4 +1,5 @@
 #
+import ntpath
 import  unittest
 
 from gistCodeUploader import GistCodeUploader
@@ -12,7 +13,15 @@ class GistCodeUploaderTest( unittest.TestCase ) :
 	def test_read_file( self ):
 		content = GistCodeUploader.read_file( "temp/Django_background_task.md" )
 		self.assertEqual( "Introduction" in content, True )
-		print(f'{ content= }')
+
+	def test_convert_code_block_to_id( self ):
+		file_path = "temp/Django_background_task.md"
+		content = GistCodeUploader.read_file( file_path )
+		file_basename = ntpath.basename( file_path )
+		id_2_code_block_info_dict, temp_markdown_text = GistCodeUploader.convert_code_block_to_id( file_basename, content )
+		print(f'{ id_2_code_block_info_dict= }')
+		print( temp_markdown_text )
+
 
 if __name__ == '__main__':
 	unittest.main()
